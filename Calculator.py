e3d04bf2418e7803e8faf4261d2a2275ef63b50f
ash <aish016@gmail.com>
2026-06-23 17:15:52 +0200

    second advancement in the logic with lists but just with addition

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,18 @@
 import math
 
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-operation = input("Enter the operation (+, -, *, /, ^, sqrt): ")
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# operation = input("Enter the operation (+, -, *, /, ^, sqrt): ")
 
-if operation == '+':
-    result = a + b
-    print(f"The result of {a} + {b} is: {result}")
+# if operation == '+':
+#     result = a + b
+#     print(f"The result of {a} + {b} is: {result}")
 
-    
+
+#rewriting to encorporate lists in addition
+
+a = input()
+parts = a.split("+")
+result = sum(int(part) for part in parts)
+print(f"The result of {a} is: {result}")
+
