af17b3bebfe0956b7fbdc947fb50c2adecbffa42
ash <aish016@gmail.com>
2026-06-23 16:50:20 +0200

    rebranding myself to thinking and building from scratch

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+import math
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /, ^, sqrt): ")
+
+if operation == '+':
+    result = a + b
+    print(f"The result of {a} + {b} is: {result}")
+
+    
